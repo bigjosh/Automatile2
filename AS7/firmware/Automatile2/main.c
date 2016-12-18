d4dc7da9ea7b62b3a00ceec73af65ff1d4a8ff33
Josh Levine <github@junk.josh.com>
2016-12-18 15:32:18 -0500

    Let's get this party started with a blinking LED!
    Just reorganizing into an AS7 project, coping existing code over.

---
+++ AS7/firmware/Automatile2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Automatile2.c
+ *
+ * Created: 12/18/2016 3:22:13 PM
+ * Author : passp
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
