42713e33fd142a3102743cb0855e86c9d7b09112
Josh Levine <github@junk.josh.com>
2017-01-05 08:25:14 -0500

    Good base for single diode per side design with all components created

--- AS7/TestDualDiode/TestDualDiode/main.c
+++ AS7/TestDualDiode/TestDualDiode/main.c
@@ -15,6 +15,8 @@
 #define REF_V (1.1)                  // Value of voltage reference
 #define THRESHOLD_V (0.600)          // Value of a detected signal from an LED in volts - changes with LEDs of different wavelength. 
 
+#define EDGE_COUNT 6                 // Number of connected edges. Must be 1-6. Edges connected to pins PA0-PA5
+
 uint8_t threshold;              // Value for detected signal measured at an 8-bit integer relative to Vcc. Updated periodically based on current Vcc to reflect dropping battery.
 
 // Print as a serial bit stream on PB2 - Each bit is 17us, just about 38400 baud - LSB first - for debugging. 
@@ -62,36 +64,47 @@ void sprint( uint8_t x ) {
 
 void setThreshold(void) {       // Set the threshold value based on current Vcc. Assumes ADC is enabled
     
-    ADMUX = 0b00100001;     // Select internal 1.1V reference to measure
-   
-    //ADMUX = 0b00000000;     // Select internal 1.1V reference to measure
+    PORTB |= _BV(PORTB0);
         
+    ADMUX = 0b00100001;     // Select internal 1.1V reference to measure
+    
+    _delay_ms(1);           // After switching to internal voltage reference the ADC requires a settling time of 1ms before
+                            // measurements are stable. Conversions starting before this may not be reliable. The ADC must
+                            // be enabled during the settling time.
+     
     ADCSRA |=  _BV(ADSC);        // Start conversion, clear previous complete flag, preserve enable
               
     while ( ADCSRA & _BV(ADSC));      // Wait for conversion to complete;
-            
-    // Do it again because we switched reference and need time to stabilize. 
-    // TODO: Check if we care for the number of bits we need.
-
-    ADCSRA |=  _BV(ADSC);        // Start conversion, clear previous complete flag, preserve enable
-
-    while ( ADCSRA & _BV(ADSC));      // Wait for conversion to complete;
-
-    //sprint(ADCH);
-                           
-    uint8_t a = ADCH;       // V = 1.1/Vcc
+    
+    uint8_t a=ADCH;
         
-    //if (a>vh) vh=a;
-    //if (a<vl) vl=a;
+    // a = value returned from ADC when measuring internal 1.1V source. 0-255.
     
-    //sprint(vh);
-    //sprint(vl);
-        
-    threshold = ((uint8_t) ( (THRESHOLD_V/REF_V) * 255.0 )) * a;       // This is an int multiply. Float calculation is static. Don't loose precision to rounding errors.
+    // a= (VREF/Vcc)*255 
+    // a/255 = VREF/Vcc
+    // (a*Vcc)/255 = VREF
+    // a*Vcc = VREF * 255
+    // Vcc = (VREF*255)/a
+       
+    // float vcc = (REF_V*255.0)/a;
+    // TODO: Might want this someday for low battery warning
     
-    ADCSRA &= ~_BV(ADEN);        // Disable ADC  - Force a long conversion on next pass becuase we changed the REF
+    // t = the ADC value that maps to the voltage THRESHOLD_V
     
-    //sprint( threshold);
+    // t = (THRESHOLD_V/Vcc)*255
+    // t = (THRESHOLD_V/((VREF*255)/a))*255
+    // t = (THRESHOLD_V * 255) / ((VREF*255)/a)
+    // t = (THRESHOLD_V * 255) * ( a / (VREF*255) )
+    // t = (THRESHOLD_V * 255 *  a)  / (VREF*255) )
+    // t = (THRESHOLD_V *  a)  / VREF )
+    // t = (THRESHOLD_V/VREF) *  a
+    
+    uint8_t t = ((THRESHOLD_V/REF_V) * a);          // Note that we have to do this math in floats since otherwise the scaling factor would round down to 0. 
+    
+    // TODO: Floats slow. Do in ints. Worth it? We only do this very, very rarely. 
+    
+    threshold = t;                                  // Save in global variable - we will use it on every scan.    
+   
 }
 
 uint16_t refreshThresholdCountdown; // TODO: Put countdown in dedicated register during sleep so no memory load needed. Maybe ok to use an int8? 255* 250ms = ~ once per minute
@@ -102,24 +115,23 @@ uint16_t refreshThresholdCountdown; // TODO: Put countdown in dedicated register
 
 inline uint8_t quickscan(void) {
     
-    //PORTB |= _BV(PORTB0);
+    PORTB |= _BV(PORTB0);
     
-    ADCSRA|= _BV(ADEN);        // Enable ADC
+    //ADCSRA|= _BV(ADEN);        // Enable ADC
     
     // TODO: Use freerunning mode, unroll loop, lockstep if we need it, tuck countdown calculation into the first 25 cycle delay
         
-    for( uint8_t edge=0; edge<6; edge++) {     
+    uint8_t edge = EDGE_COUNT;
+    
+    while (edge--) {
 
         ADMUX= edge;          // Select pin, use Vcc as reference
             
         ADCSRA |=  _BV(ADSC);        // Start conversion
     
-        PORTB |= _BV(PORTB1);
 
         while ( ADCSRA & _BV(ADSC));      // Wait for conversion to complete;
-        PORTB &= ~_BV(PORTB1);      // Turn off indication LED   
-        
-        
+                
         uint8_t v = ADCH;
         
         //sprint(edge);
@@ -130,7 +142,7 @@ inline uint8_t quickscan(void) {
         }
     }
 
-    ADCSRA &= ~_BV(ADEN);        // Disable ADC
+    //ADCSRA &= ~_BV(ADEN);        // Disable ADC
     
     /*
     if (!--refreshThresholdCountdown) {     
@@ -138,7 +150,7 @@ inline uint8_t quickscan(void) {
     }
     */
     
-//    PORTB &= ~_BV(PORTB0);      // Turn off indication LED
+    PORTB &= ~_BV(PORTB0);      // Turn off indication LED
         
     return(0);
                 
@@ -147,33 +159,41 @@ inline uint8_t quickscan(void) {
 uint8_t rx_bits;
 
 // Scan the 6 input lines and see which ones are above threshold
-// Assumes ADC already enabled
 
 void scan(void) {
     
-    PORTB |= _BV(PORTB0);
+    //PORTB |= _BV(PORTB0);
           
     rx_bits = 0;
     
-    for( uint8_t edge=0; edge<3; edge++) {     // TODO: CHeck more than 1
-
-        ADMUX= edge;          // Select pin, use Vcc as reference
+    // TODO: Reverse order so bit0 is PA0
+    // TODO: Run in continuous mode 
+    
+    uint8_t edge = EDGE_COUNT;
+    
+    while (edge--) {
+                
+        rx_bits <<=1;
+                
+        ADMUX=edge;
         
         ADCSRA |=  _BV(ADSC);        // Start conversion
+        
+        //PORTB |= _BV(PORTB0);      // Turn off indication LED    
+        //PORTB &= ~_BV(PORTB0);      // Turn off indication LED
+        
         
         while ( ADCSRA & _BV(ADSC));      // Wait for conversion to complete;
         
         uint8_t v = ADCH;
-        
-        sprint(edge);
-        sprint(v);
-        
+                
         if ( v > threshold ) {
             rx_bits |= 1;
         }
         
-        rx_bits <<=1;
     }
+    
+    // TODO: Unroll this loop, use T bit to rotate the new bit in. 
     
     
     /*
@@ -183,7 +203,7 @@ void scan(void) {
     
     */
     
-    PORTB &= ~_BV(PORTB0);      // Turn off indication LED
+    //PORTB &= ~_BV(PORTB0);      // Turn off indication LED
         
 }
 
@@ -200,9 +220,11 @@ int main(void)
     DIDR0 = 0b00111111;     // Disable digital input on all IR LED pins to save power since we will be floating
         
     ADCSRB |= _BV( ADLAR);  // Left justify result so we only need to read the high 8 bits.
-        
-    setThreshold();
+            
+    ADCSRA|= _BV(ADEN);        // Enable ADC 
     
+    setThreshold();
+                
     /*
     while (1) {
         PORTA |=_BV(1);
@@ -211,14 +233,10 @@ int main(void)
         _delay_ms(200);
     }
     */
-    //PORTA |= _BV(2);        // Pull-up n pin PA2
-    
-    
-    // Input D to A on PA0
-        
       
     while (1) 
     {
+        /*
           PORTB |= _BV(PORTB0);      // Turn off indication LED
 
          if (quickscan()) {
@@ -232,8 +250,48 @@ int main(void)
          }
          
          PORTB &= ~_BV(PORTB0);      // Turn off indication LED
+         */
+        
+        PORTB |= _BV(PORTB1);       // IR ON
+        _delay_ms(400);
+        
+        //_delay_us(10);
 
+        DDRA = 0b111111;             // Purge stranded charge
+        DDRA = 0;
+        
+        scan();
+        
+        if ( rx_bits & _BV(0) ) {
+          PORTB |= _BV(PORTB0);         // Turn off indication LED                                   
+        } else {
+          PORTB &= ~_BV(PORTB0);        // Turn off indication LED
+        }            
+        
+        PORTB &= ~_BV(PORTB1);       // IR OFF
+        _delay_ms(200);
+        
+        DDRA = 0b111111;             // Purge stranded charge
+        DDRA = 0;
+        
+        //_delay_us(10);
+        
+        scan();
+        
+        if ( rx_bits & _BV(0) ) {
+            PORTB |= _BV(PORTB0);         // Turn off indication LED
+            } else {
+            PORTB &= ~_BV(PORTB0);        // Turn off indication LED
+        }
+       
+        //sprint(threshold);
+        //sprint(rx_bits);
+        
+        //setThreshold();
         
     }
+    
+    ADCSRA &= ~_BV(ADEN);        // Disable ADC  
+
 }
 
